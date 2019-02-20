057ab91968403c203b11085460ac76f97f50c659
Omar De Leo <omar.deleo@gmail.com>
2019-02-20 13:47:38 -0500

    Delete mutation observer

--- firebase.js
+++ firebase.js
@@ -5,33 +5,3 @@ firebase.initializeApp({
 });
 
 var db = firebase.firestore();
-
-function submitScore(score) {
-  db.collection("scores").doc("top-score").set({
-    score: score,
-  })
-  .then(function() {
-      console.log("Document successfully written!");
-  })
-  .catch(function(error) {
-      console.error("Error writing document: ", error);
-  });
-}
-
-const score = document.querySelector('.score');
-
-var mutationObserver = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
-    if (mutation.target.className.includes('final-score')) {
-      console.log("GAME OVER");
-      let finalScore = parseInt(mutation.target.innerHTML);
-      console.log("final score", finalScore);
-      submitScore(finalScore);
-      score.classList.remove('final-score');
-    }
-  });
-});
-
-mutationObserver.observe(score, {
-  attributes: true,
-});
