974b0efaa631c1c8cbefd20df8fde495bb53b114
Omar De Leo <omar.deleo@gmail.com>
2019-02-20 11:54:45 -0500

    Configure firebase

--- firebase.js
+++ firebase.js
@@ -1,3 +1,24 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyCvQX1CZOtwrgaQhFw_n0ixptfH5jmcKoQ",
+  authDomain: "top-score-97007.firebaseapp.com",
+  projectId: "top-score-97007"
+});
+
+var db = firebase.firestore();
+
+function submitScore(score) {
+  db.collection("scores").add({
+    score: score,
+  })
+  .then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  });
+}
+
+
 const score = document.querySelector('.score');
 
 var mutationObserver = new MutationObserver(function(mutations) {
@@ -6,6 +27,7 @@ var mutationObserver = new MutationObserver(function(mutations) {
       console.log("GAME OVER");
       let finalScore = parseInt(mutation.target.innerHTML);
       console.log("final score", finalScore);
+      submitScore(finalScore);
       score.classList.remove('final-score');
     }
   });
